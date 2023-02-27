8a39aaf4b2e2b703a3d6147be16c79737129e840
tehelusivepanda <t3helusivepanda@gmail.com>
2023-02-27 18:21:51 -0500

    Basic setup

--- src/main/java/com/moaningjewelry/MoaningJewelryConfig.java
+++ src/main/java/com/moaningjewelry/MoaningJewelryConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.moaningjewelry;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface MoaningJewelryConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
